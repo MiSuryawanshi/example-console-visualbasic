2acda198a585658d586272432da9f7c22d8cba3d
Tom Flídr <tomflidr@gmail.com>
2019-09-20 18:08:33 +0200

    desharp example console app with running tests

---
+++ Program.vb
@@ -0,0 +1,17 @@
+﻿Imports Desharp
+
+Module Program
+
+	Sub Main()
+
+		Dim dlTest = New Tests.DumpingAndLoging()
+		dlTest.TestAll()
+
+		Dim eTest = New Tests.ExceptionsRendering()
+		eTest.TestAll()
+
+		Console.ReadLine()
+
+	End Sub
+
+End Module
